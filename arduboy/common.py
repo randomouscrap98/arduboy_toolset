c01a750d57c306e6a4bad982718371f5a4931fba
Carlos Sanchez <randomouscrap98@aol.com>
2023-09-04 17:34:39 -0400

    Tests for arduboy.common

--- arduboy/common.py
+++ arduboy/common.py
@@ -1,31 +1,53 @@
 from arduboy.constants import *
 
-# Pad data to a multiple of the given size. For instance, if data is 245 but multsize is
-# 256, it is padded up to 256 with the given pad data. If data is 400 and multsize is 256,
-# it is padded up to 512
 def pad_data(data: bytearray, multsize, pad = b'\xFF'):
+    """Pad data to a multiple of the given size.
+
+    For instance, if data is 245 but multsize is 256, it is padded up to 256 with the given pad data.
+    If data is 400 and multsize is 256, it is padded up to 512
+
+    Returns: 
+        padded data
+    """
     if len(data) % multsize: 
         data += pad * (multsize - (len(data) % multsize))
     return data
 
-# Return the amount of padding to add to the given length of data if you wish for the 
-# given alignment. For instance, if length is 245 but alignment is 256, 11 is returned
-def pad_size(length, alignment):
+def pad_size(length: int, alignment: int):
+    """Compute amount of padding to add to the given length of data for the given alignment
+    
+    For instance, if length is 245 but alignment is 256, 11 is returned
+    
+    Returns:
+        amount of padding to make length be aligned to given alignment
+    """
     if length % alignment:  # It is misaligned, to get it aligned, must know the difference
         return alignment - (length % alignment)
     else: # It is exactly aligned
         return 0 
 
-# Get the bit at position (1 or 0)
-def bytebit(byte, pos):
+def bytebit(byte: int, pos: int):
+    """Get the bit at position
+    
+    Returns:
+        The bit (1 or 0)
+    """
     return (byte >> pos) & 1
 
-# Return integer as hex string with the given number of hex characters (prepadded with 0)
-def int_to_hex(integer, hexchars):
+def int_to_hex(integer: int, hexchars: int):
+    """Get hexstring for given integer (without 0x)
+
+    Returns:
+        Integer as hex with given amount of characters (prepadded with 0)
+    """
     return hex(integer).replace("0x", "").upper().zfill(hexchars)
 
-# Find the number of unused pages at the end of the given data block
-def count_unused_pages(data):
+def count_unused_pages(data: bytearray):
+    """Get the number of unused pages (Arduboy/FX size) at the end of the given data block
+    
+    Returns:
+        count of unused pages
+    """
     last_FF_index = 0 # Loop will exit without assignment if ALL are 0xFF
     for i in range(len(data) - 1, -1, -1):
         if data[i] != 0xFF:
--- arduboy/test/test_common.py
+++ arduboy/test/test_common.py
@@ -1,5 +1,6 @@
 import unittest
 from arduboy.common import *
+from arduboy.constants import *
 
 class TestCommon(unittest.TestCase):
 
@@ -8,12 +9,92 @@ class TestCommon(unittest.TestCase):
         result = pad_data(data, 5)
         self.assertEqual(data, result)
 
+    def test_pad_data_multiple(self):
+        data = bytearray([1,2,3,4,5,6])
+        result = pad_data(data, 3)
+        self.assertEqual(data, result)
+
     def test_pad_data_smaller(self):
         data = bytearray([1,2,3,4,5])
         expected = bytearray([1,2,3,4,5,0xFF])
         result = pad_data(data, 3)
         self.assertEqual(expected, result)
 
+    def test_pad_data_larger(self):
+        data = bytearray([1,2,3,4,5])
+        expected = bytearray([1,2,3,4,5,0xFF,0xFF])
+        result = pad_data(data, 7)
+        self.assertEqual(expected, result)
+
+    def test_pad_data_special(self):
+        data = bytearray([1,2,3,4,5,6,7,8,9])
+        expected = bytearray([1,2,3,4,5,6,7,8,9,69,69,69])
+        result = pad_data(data, 4, int.to_bytes(69, 1, 'little'))
+        self.assertEqual(expected, result)
+    
+    def test_padsize(self):
+        tests = [
+            (5, 5, 0),
+            (5, 6, 1),
+            (5, 3, 1),
+            (9, 4, 3),
+        ]
+        for length, align, expected in tests:
+            with self.subTest(length = length, align = align, expected = expected):
+                self.assertEqual(pad_size(length, align), expected)
+
+    def test_bytebit(self):
+        tests = [
+            (0b11001010, 0, 0),
+            (0b11001010, 1, 1),
+            (0b11001010, 2, 0),
+            (0b11001010, 3, 1),
+            (0b11001010, 4, 0),
+            (0b11001010, 5, 0),
+            (0b11001010, 6, 1),
+            (0b11001010, 7, 1),
+        ]
+        for num, pos, expected in tests:
+            with self.subTest(num = num, pos = pos, expected = expected):
+                self.assertEqual(bytebit(num, pos), expected)
+
+    def test_int_to_hex(self):
+        tests = [
+            (5, 1, "5"),
+            (5, 2, "05"),
+            (5, 4, "0005"),
+            (15, 1, "F"),
+            (15, 2, "0F"),
+            (255, 2, "FF"),
+            (255, 4, "00FF")
+        ]
+        for i, len, expected in tests:
+            with self.subTest(i = i, len = len, expected = expected):
+                self.assertEqual(int_to_hex(i, len), expected)
+
+    def test_count_unused_pages_small(self):
+        data = bytearray([1,2,3,4,5])
+        self.assertEqual(0, count_unused_pages(data))
+
+    def test_count_unused_pages_noalignment(self):
+        data = bytearray([1,2,3,4,5])
+        data += b'\xFF' * FX_PAGESIZE
+        self.assertEqual(1, count_unused_pages(data))
+
+    def test_count_unused_pages_off_by_1_nopage(self):
+        data = bytearray([1,2,3,4,5])
+        data += b'\xFF' * (FX_PAGESIZE - 1)
+        self.assertEqual(0, count_unused_pages(data))
+
+    def test_count_unused_pages_off_by_1_withpage(self):
+        data = bytearray([1,2,3,4,5])
+        data += b'\xFF' * (FX_PAGESIZE * 2 - 1)
+        self.assertEqual(1, count_unused_pages(data))
+
+    def test_count_unused_pages_off_by_1_overpage(self):
+        data = bytearray([1,2,3,4,5])
+        data += b'\xFF' * (FX_PAGESIZE * 2 + 1)
+        self.assertEqual(2, count_unused_pages(data))
 
 if __name__ == '__main__':
     unittest.main()
