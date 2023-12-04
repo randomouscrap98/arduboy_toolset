2ef2e5974745074a37555b4262fc37fc436ed597
Carlos Sanchez <randomouscrap98@aol.com>
2023-12-03 21:15:31 -0500

    The full process on their end works!

--- adhoc/badh_json_to_bin.py
+++ adhoc/badh_json_to_bin.py
@@ -1,10 +1,20 @@
+import sys
+import os
+
+# All because vscode debugger or whatever
+thisdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(thisdir)
+os.chdir(thisdir)
+sys.path.append(parentdir)  # Add the parent directory to the Python path
+
+import arduboy.arduhex
 from arduboy.bloggingadeadhorse import *
-import gui_common
+from gui_common import *
 import json
 
 # First, gotta get that json
 print("Downloading cart meta...")
-cartmeta = gui_common.get_official_cartmeta(force = True)
+cartmeta = get_official_cartmeta(force = True)
 print(f"Cartmeta total: {len(cartmeta)}")
 
 with open("badh_last.json", "w") as f:
@@ -12,7 +22,7 @@ with open("badh_last.json", "w") as f:
 
 # Then, gotta compute the update against an empty nothing
 print("Computing update...")
-update = compute_update([], cartmeta)
+update = compute_update([], cartmeta, arduboy.arduhex.DEVICE_ARDUBOYFX)
 print(f"Updates: {len(update[UPKEY_UPDATES])} | New: {len(update[UPKEY_NEW])} | Current: {len(update[UPKEY_CURRENT])} | Unmatched: {len(update[UPKEY_UNMATCHED])}")
 
 # Now, just pull all the little bits out of the update and the new fields and make a csv
@@ -20,4 +30,14 @@ print("Creating csv...")
 csv = create_csv(update[UPKEY_NEW] + [f[1] for f in update[UPKEY_UPDATES]])
 
 with open("badh_csvsubmit.csv", "w") as f:
-    f.write(csv)
+    f.write(csv.replace(BADH_EOL, "\n"))
+
+# And now we post some data to the websiiiteee
+print("Posting CSV, expecting bin...")
+result = get_official_bin(csv)
+print(f"Bin size: {len(result)}")
+
+with open("badh_finalresult.bin", "wb") as f:
+    f.write(result)
+
+print("All done!")
